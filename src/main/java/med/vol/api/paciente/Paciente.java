4fb0f509f4d075cbed64c207f840b38b799c6abb
Tales <taleskodama@gmail.com>
2025-12-16 11:07:06 -0300

    feat: implement soft delete for pacients and information update

--- src/main/java/med/vol/api/paciente/Paciente.java
+++ src/main/java/med/vol/api/paciente/Paciente.java
@@ -2,6 +2,7 @@ package med.vol.api.paciente;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -26,11 +27,29 @@ public class Paciente {
     @Embedded
     private Endereco endereco;
 
+    private Boolean ativo;
+
     public Paciente(DadosCadastroPaciente dados) {
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
         this.cpf = dados.cpf();
         this.endereco = new Endereco(dados.endereco());
+    }
+
+    public void AtualizarInformacoes(DadosAtualizacaoPaciente dados) {
+        if(dados.nome() != null){
+            this.nome = dados.nome();
+        }
+        if(dados.telefone() != null){
+        this.telefone = dados.telefone();
+        }
+        if(dados.endereco() != null){
+        this.endereco.atualizarInformacoes(dados.endereco());
+        }
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
