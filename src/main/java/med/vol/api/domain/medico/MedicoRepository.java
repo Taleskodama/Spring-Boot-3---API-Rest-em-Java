8e81c9bc7121f15a839aa02fe464a0b496fa77ee
Tales <taleskodama@gmail.com>
2025-12-30 23:06:21 -0300

    feat: Implement business rule validations

--- src/main/java/med/vol/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/vol/api/domain/medico/MedicoRepository.java
@@ -31,4 +31,12 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
          """)
   Medico escolherMedicoAleatorioLivreNaData(
       Especialidade especialidade, @NotNull @Future LocalDateTime data);
+
+  @Query("""
+        select m.ativo
+        from Medico m
+        where
+        m.id = :id
+        """)
+  Boolean findAtivoById(Long idMedico);
 }
